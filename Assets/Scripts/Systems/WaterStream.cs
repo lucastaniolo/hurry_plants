9442a090210343bcc81e1317dce4ffe4845f141a
Taniolo <lucastaniolo@gmail.com>
2019-10-05 16:10:55 -0300

    Create WaterStream

---
+++ Assets/Scripts/Systems/WaterStream.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class WaterStream : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    private GroundMovement groundMovement;
+
+    private void OnTriggerEnter(Collider other) => groundMovement = other.GetComponent<GroundMovement>();
+    private void OnTriggerExit(Collider other) => groundMovement = null;
+
+    private void FixedUpdate()
+    {
+        if (groundMovement == null) return;
+        groundMovement.Move(transform.forward * velocity);
+    }
+}
