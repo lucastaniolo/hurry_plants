bc9ab4e4c0d71979cfc1682993d6ae0abb5e7344
Lucas Taniolo <48696056+ltaniolo@users.noreply.github.com>
2019-10-04 22:31:38 -0300

    Update Bomb.cs

--- Assets/Scripts/Actors/Bomb.cs
+++ Assets/Scripts/Actors/Bomb.cs
@@ -1,3 +1,14 @@
+// Add timer to explode the bomb
+// Countdown starts as soon as a player picks up the bomb
+// Time to explode varies, designer must be able to set it via inspector
+// Countdown must have a visual feedback (not defined yet)
+// Bomb respawn after explosion, unless it explodes a crate
+// Explosion affects nearby actors:
+// - Player: kill and respawn
+// - HumanNPC: kill and respawn
+// - Other bomb: explode and respawn
+// - Crate (objective): destroy
+
 ﻿public class Bomb : PickUpElement<Crate>
 {
 }
