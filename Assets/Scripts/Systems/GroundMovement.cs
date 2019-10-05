03334cc1008fe7ba74ba81cee91b21fb9a007006
Lucas Taniolo <48696056+ltaniolo@users.noreply.github.com>
2019-10-04 22:53:05 -0300

    Update GroundMovement.cs

--- Assets/Scripts/Systems/GroundMovement.cs
+++ Assets/Scripts/Systems/GroundMovement.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 
+// TODO: water stream
+// Create system that influence GroundMovement speed
+// Behaviour is similar to a treadmill
+// Inspector should have a velocity property, direction is defined by object rotation
 
 public class GroundMovement : BaseMovement
 {
